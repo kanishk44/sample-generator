1956b59a4be3da9c6802cb9d7d98ad4a6e6bdfed
kanishk44 <ks468@njit.edu>
2022-11-07 21:17:44 -0500

    Main app file where program runs

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,26 @@
 """This is the main startup of the app"""
+from app.file_ops import FileOperations
+from app.pandas_ops import *
 
 
 def main():
     """This is the main function that is run"""
+    abs_path = FileOperations.get_calculate_file_path(Config.data_directory, Config.file_name)
+    out_path = FileOperations.get_calculate_file_path(Config.data_directory, Config.sample_files_output)
+    df = PandasCsvRead.get_df(abs_path)
+    count = PandasCount.get_count(df)
+    file_names = FileNameGenerator.get_file_names(Config.z_scores, Config.margin_of_error, df)
+    sample_sizes = []
 
+    for z in Config.z_scores.values():
+        for e in Config.margin_of_error:
+            sample_sizes.append(SampleSizeGenerator.get_sample_size(z, e, count))
 
-if __name__ == '__main__':
-    """This causes the hello world function to be called if this is the __main__ top level of code"""
-    main()
+    for n in sample_sizes:
+        for file_name in file_names:
+            sample = SampleGenerator.get_sample(df, n)
+            FileOperations.get_csv(sample, out_path+file_name)
+
+    if __name__ == '__main__':
+        """This causes the hello world function to be called if this is the __main__ top level of code"""
+        main()
