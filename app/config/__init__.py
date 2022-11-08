9618175a54eb0b3858cec14e3bb442c3a59eedf7
kanishk44 <ks468@njit.edu>
2022-11-07 21:15:07 -0500

    Configurations for program

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -4,9 +4,11 @@
 class Config:
     """Program Configure"""
     data_directory = "data"
-    data_file_name = "state_population.csv"
+    file_name = "state_population.csv"
     data_field = "pop"
     sample_files_output = "sample_files_output"
-    margin_of_error = (.01, .05, .10)
-    z_scores = (1.28, 1.44, 1.65, 1.96, 2.58)
+    margin_of_error = [.01, .05, .10]
+    z_scores = {"80": 1.28, "85": 1.44, "90": 1.65, "95": 1.96, "99": 2.58}
     rounding_precision = 2
+    std = .5
+
