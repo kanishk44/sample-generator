2bc7a644c6666c0fe46b818797ae1cb0cdc78962
kanishk44 <ks468@njit.edu>
2022-11-07 21:15:55 -0500

    Math operations: Squaring, rounding

---
+++ app/math_ops/__init__.py
@@ -0,0 +1,18 @@
+"""Math operations"""
+from app.config import Config
+
+
+class RoundingToAppConfig:
+    """Round the value to app's rounding precision configuration"""
+    @staticmethod
+    def get_result(value):
+        """Get rounding precision"""
+        return round(value, Config.rounding_precision)
+
+
+class SquareNum:
+    """Square the number"""
+    @staticmethod
+    def get_square(value):
+        return pow(value, 2)
+
